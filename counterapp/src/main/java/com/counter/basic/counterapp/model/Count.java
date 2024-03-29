1528c343b54ca3435e36517a25f26a7fd40fedd4
qpjiq123@gmail.com <qpjiq123@gmail.com>
2024-03-30 02:07:17 +0900

    init counterapp

---
+++ counterapp/src/main/java/com/counter/basic/counterapp/model/Count.java
@@ -0,0 +1,12 @@
+package com.counter.basic.counterapp.model;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+public class Count {
+    private int count;
+}
