6a79075b96bb3243acc5db23e8230335477a5ddd
qpjiq123@gmail.com <qpjiq123@gmail.com>
2024-04-02 01:08:27 +0900

    react.base.url 확인

--- counterapp/src/main/java/com/counter/basic/counterapp/WebConfig.java
+++ counterapp/src/main/java/com/counter/basic/counterapp/WebConfig.java
@@ -1,5 +1,6 @@
 package com.counter.basic.counterapp;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
