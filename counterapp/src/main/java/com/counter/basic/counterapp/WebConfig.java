57e1b43a38f0d283bf0f088deca188bcb22df451
qpjiq123@gmail.com <qpjiq123@gmail.com>
2024-04-06 00:19:45 +0900

    cors

--- counterapp/src/main/java/com/counter/basic/counterapp/WebConfig.java
+++ counterapp/src/main/java/com/counter/basic/counterapp/WebConfig.java
@@ -1,14 +1,11 @@
 package com.counter.basic.counterapp;
 
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-@EnableWebMvc
 public class WebConfig implements WebMvcConfigurer {
 
     @Value("${react.base.url}")
@@ -16,9 +13,10 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOrigins(reactBaseUrl, "http://localhost:3000", "http://43.203.125.28:3000")
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                .allowedHeaders("*");
+        registry.addMapping("/**") // 모든 경로에 대해
+                .allowedOriginPatterns("*") // 모든 출처 허용
+                .allowedMethods("*") // 모든 HTTP 메소드 허용
+                .allowedHeaders("*") // 모든 헤더 허용
+                .allowCredentials(true).maxAge(3600); // 쿠키를 포함한 요청 허용 및 pre-flight 캐시 시간 설정
     }
 }
--- counterapp/src/main/java/com/counter/basic/counterapp/controller/CountController.java
+++ counterapp/src/main/java/com/counter/basic/counterapp/controller/CountController.java
@@ -11,7 +11,6 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/api/count")
 @RequiredArgsConstructor
 @Slf4j
-@CrossOrigin
 public class CountController {
     private final CountService countService;
 
